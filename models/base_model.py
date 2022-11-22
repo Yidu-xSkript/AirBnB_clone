0e0b5c32745ebafb9e164026e7927e9e6aaebcf8
Yidu-xSkript <yidu95@gmail.com>
2022-11-22 15:58:18 +0300

    File Storage Functionalities Implemented! + Test Implemented

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import datetime
 import uuid
-# from models import storage
+from models import storage
 
 class BaseModel():
     def __init__(self, *args, **kwargs):
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
-        # self.__dict__.update(kwargs)
 
         if len(kwargs) > 0:
             self.__dict__.update(kwargs)
@@ -16,22 +15,38 @@ class BaseModel():
             if '__class__' in self.__dict__:
                 self.__dict__.pop('__class__')
            
-            if type(self.__dict__['created_at']) == str and type(self.__dict__['updated_at']) == str:
+            if type(self.__dict__['created_at']) == str:
                 self.__dict__['created_at'] = datetime.datetime.strptime(self.__dict__['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            if type(self.__dict__['updated_at']) == str:
                 self.__dict__['updated_at'] = datetime.datetime.strptime(self.__dict__['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
             
         # If it's a new instance (not from a dictionary representation), add a call to the method "new(self)" on "storage".
-        # storage.new()
+        storage.new(self)
 
     def __str__(self) -> str:
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """Update the database / storage.
+        - update the updated_at attribute.
+        - save data to JSON file.
+        """
         self.updated_at = datetime.datetime.now()
-        # storage.save()
+        self.to_dict()
+        storage.save()
 
     def to_dict(self) -> dict:
+        """Modify Dictionary and return the modified dictionary
+
+        Returns:
+            dict: self.__dict__
+        """
         self.__dict__['__class__'] = self.__class__.__name__
-        self.__dict__['created_at'] = self.__dict__['created_at'].strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.__dict__['updated_at'] = self.__dict__['updated_at'].strftime("%Y-%m-%dT%H:%M:%S.%f")  
+
+        if type(self.__dict__['created_at']) != str:
+            self.__dict__['created_at'] = self.__dict__['created_at'].strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+        if type(self.__dict__['updated_at']) != str:
+            self.__dict__['updated_at'] = self.__dict__['updated_at'].strftime("%Y-%m-%dT%H:%M:%S.%f")  
+
         return self.__dict__
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
+import json
+from pathlib import Path
 
 class FileStorage():
-    def __file_path():
+    def __file_path(self) -> str:
         """
         string - path to the JSON file (ex: file.json)
         """        
-        pass
+        return "file.json"
 
-    def __objects():
+    def __objects(self) -> dict:
         """
         empty but will store all objects by <class name>.id 
         (ex: to store a BaseModel object with id=12121212, the key will be BaseModel.12121212)
         """        
-        pass
+        return self.__dict__
 
-    def all(self):
+    def all(self) -> dict:
         """
         returns the dictionary __objects
         """
-        pass
+        return self.__objects()
 
     def new(self, obj):
         """ 
@@ -26,17 +28,21 @@ class FileStorage():
         Args:
             obj (_type_): _description_
         """
-        pass
+        self.__dict__[obj.__class__.__name__ + '.' + obj.__dict__['id']] = obj.to_dict()
 
     def save(self):
         """
         serializes __objects to the JSON file (path: __file_path)
         """
-        pass
+        with open(self.__file_path(), 'w') as fp:
+            json.dump(self.__objects(), fp)
 
     def reload(self):
         """
         deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; 
         otherwise, do nothing. If the file doesn't exist, no exception should be raised)
         """
-        pass
+        file = Path(self.__file_path())
+        if file.is_file():
+            with open(self.__file_path(), "r") as fp:
+                self.__dict__ = json.load(fp)
